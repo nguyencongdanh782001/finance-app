2df49f306cd1e57d77aaabfaac22f9bdc2c06a1f
Danh Nguyen <your-email@gmail.com>
2026-03-03 16:21:57 +0700

    update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react";
 import HomeContainer from "@/container/HomeContainer";
 
 export default function Home() {
-  return <HomeContainer />;
+  return (
+    <Suspense>
+      <HomeContainer />
+    </Suspense>
+  );
 }
